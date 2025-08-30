321597be846ad8269cfc48ca5b29046bfbd3f37b
LSO182 <sanchezolmedoluciano@gmail.com>
2025-08-30 14:34:37 -0300

    footer text

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="main-footer bg-primary"></div>
+  <div class="main-footer bg-primary">
+    <div class="container">
+      <div class="row justify-content-center align-items-center">
+        <p class="text-white my-3 text-center">
+          © 2025 - Todos los derechos reservados | Kargoo SPA | RUT 78209183-2
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup></script>
